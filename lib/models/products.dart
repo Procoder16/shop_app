6c20998374f67a3cf7cfaa60613f83ad7bf5c3dc
Procoder16 <smsoumik1608@gmail.com>
2022-01-13 02:16:55 +0530

    products model added

---
+++ lib/models/products.dart
@@ -0,0 +1,17 @@
+class Products {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavourite;
+
+  Products({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    required this.isFavourite,
+  });
+}
---
+++ lib/screens/product_overview_screen.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: use_key_in_widget_constructors
+import 'package:flutter/material.dart';
+
+class ProductOverviewScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
